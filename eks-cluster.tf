fe0ade755eefd6eaf780a7713468f0a64809dc26
Narayan Parvatikar <narayan.parvatikar@nvisionx.ai>
2025-12-30 13:54:07 +0530

    fix: update EKS module arguments for v21.x
    - cluster_name → name
    - cluster_version → kubernetes_version
    - cluster_endpoint_* → endpoint_*
    - cluster_addons → addons

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -6,22 +6,22 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
+  name               = var.cluster_name
+  kubernetes_version = var.cluster_version
 
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
 
-  cluster_endpoint_private_access          = var.cluster_endpoint_private_access
-  cluster_endpoint_public_access           = var.cluster_endpoint_public_access
-  cluster_endpoint_public_access_cidrs     = var.cluster_endpoint_public_access_cidrs
-  enable_irsa                              = var.enable_irsa
-  create_iam_role                          = var.create_iam_role
-  iam_role_arn                             = var.cluster_iam_role_arn
-  eks_managed_node_groups                  = var.eks_managed_node_groups
+  endpoint_private_access      = var.cluster_endpoint_private_access
+  endpoint_public_access       = var.cluster_endpoint_public_access
+  endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
+  enable_irsa                  = var.enable_irsa
+  create_iam_role              = var.create_iam_role
+  iam_role_arn                 = var.cluster_iam_role_arn
+  eks_managed_node_groups      = var.eks_managed_node_groups
   enable_cluster_creator_admin_permissions = true
 
-  cluster_addons = {
+  addons = {
     coredns                = {}
     eks-pod-identity-agent = {}
     kube-proxy             = {}
